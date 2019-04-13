91b4678d876899ca2efb561bee88da0a8ec39457
Vyom Agarwal <42782904+vyster@users.noreply.github.com>
2019-04-13 15:42:10 +0530

    Update Template.cpp

--- GFG MDCQ/Template.cpp
+++ GFG MDCQ/Template.cpp
@@ -7,7 +7,12 @@ int main() {
         int n,m; cin>>n>>m;int a[n];for(int i=0; i<n; i++)cin>>a[i];
         
         for(int i=0; i<n; i++)
-            for(int j=i+1; j<n; j++)
+            for(int j=i+1; j<n; j++){
+		    
+		    if(a[i]==a[j]){
+			    
+		    }
+	    }
             
         
         
