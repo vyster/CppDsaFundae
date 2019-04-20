0e577c1232c3b4ee8f025144dfcb592906d62aa9
Vyom Agarwal <42782904+vyster@users.noreply.github.com>
2019-04-21 03:12:41 +0530

    Update print.cpp

--- print.cpp
+++ print.cpp
@@ -1,9 +1,23 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
-
-int main(){
-
-  cout<<'Print';
-  return 0;
+int main() {
+    int t; cin>>t;while(t--){
+        
+        int n,m; cin>>n>>m;int a[n];for(int i=0; i<n; i++)cin>>a[i];
+        
+        for(int i=0; i<n; i++)
+            for(int j=i+1; j<n; j++){
+		    
+		    
+			    
+	    }
+            
+        
+        
+        
+        for(int i=0; i<n; i++)cout<<a[i]<<endl;
+        }
+	
+	return 0;
 }
