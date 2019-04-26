3bde83fcad869acc6720c9bf5fc46f3728861149
Vyom Agarwal <42782904+vyster@users.noreply.github.com>
2019-04-27 00:34:44 +0530

    Update print.cpp

--- print.cpp
+++ print.cpp
@@ -1,14 +1,3 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-int main(){
-ccc
-  cout<<'Print';
-  return 0;
-}
-cc
-ccccc
-classifier.add(Convolution2D(96, 11, strides = (4, 4), padding = 'valid', input_shape=(227, 227, 3), activation = 'relu'))
-cccc
-ccc
