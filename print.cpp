4275bcb07b2af9674b2ef8e76e38b1f7913a6da5
Vyom Agarwal <42782904+vyster@users.noreply.github.com>
2019-06-12 00:17:12 +0530

    Update print.cpp

--- print.cpp
+++ print.cpp
@@ -22,3 +22,27 @@
     }
     
     return count;
+
+    int i = 0;
+    int j = 0;
+    int sum = 0;
+    int count = 0;
+    
+    while(i < A.size()){
+        sum = sum + A[j];
+        if((sum >= B) && (sum <= C)){
+            j++;
+        }
+        else if(sum > C){
+            i++;
+            j = i;
+            sum = 0;
+        }
+        if(j == A.size()){
+            sum = 0;
+            i++;
+            j = i;
+        }
+    }
+    
+    return count;
